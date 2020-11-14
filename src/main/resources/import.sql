e2b59fc68558966fbd9dff0590081fc9621e847b
Christan Ariel Salazar Verduga <santisimoariel@gmail.com>
2020-11-14 01:18:58 +0100

    Decorando la vista listar con estilos CSS Bootstrap

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-/*populate tables, utilizndo H2 base de datos de prueba e insertando datos en la tabla clients, es importante que este archivo se llave import.sql*/
+/*populate tables, utilizndo H2 base de datos de prueba e insertando datos en la tabla clients, es importante que este archivo se llave import.sql ya que JPA va a buscar el archivo con este nombre */
 INSERT INTO clients(id,first_name,last_name,email,create_at) VALUES(1, 'Pedro', 'Guzman','pguzman@gmail.com','2020-11-13');
 INSERT INTO clients(id,first_name,last_name,email,create_at) VALUES(2, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13');
