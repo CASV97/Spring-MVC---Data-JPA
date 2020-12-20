3eac58ba029b5a325aca2d1c3a12bdb0d461e65f
Christan Ariel Salazar Verduga <ariel-2h@hotmail.com>
2020-12-20 13:46:37 +0100

    Asociaciones: OneToMany Unidireccional - Clases Entity Factura y ItemFactura
    continuandoi con el eljemplo de facturación, el objetivo es crear la clase entity <<InvoiceLineItem>> (itemFactura), la clase <<Invoice>> (factura) por una parte está relacionada con la clase <<InvoiceLineItem>> de modo que una factura contienes muchos elementos Items, pero a la inversa la clase Item factura no tiene relación con factura por que en ningun momento de la aplicación no va a interesar cuál es la factura de un Item en particular (no tiene sentido), hacemos uso de la anotacion @JoinColumn debido a a la relacion en un solo sentido que tienen las tablas invoice e invoice_items, de esta forma se crea la clave foránea de la factura a la que corresponde en la tabla item_factura.

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Invoice.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Invoice.java
@@ -1,15 +1,20 @@
 package com.bolsadeideas.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -17,6 +22,7 @@ import javax.persistence.TemporalType;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+/*como buena practica toda clase entity debe comenzar por la implementación de la interfáz serializable*/
 @Entity
 @Table(name = "invoices")
 public class Invoice implements Serializable {
@@ -32,11 +38,28 @@ public class Invoice implements Serializable {
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	private Date createAt;
 
-	// Muchas facturas un cliente, Lazy para evitar que cuando se hace una consulta
-	// de la Invoice tambien haga una consulta del Client
+	/*
+	 * Muchas facturas un cliente, Lazy para evitar que cuando se hace una consulta
+	 * de la Invoice tambien haga una consulta del Client
+	 */
 	@ManyToOne(fetch = FetchType.LAZY)
-
 	private Client client;
+
+	/*
+	 * obtener los items de la factura, tenemos que indicar cuando la relación es en
+	 * un solo sentido cual es la llave foránea que va a relacionar en la tabla
+	 * (invoice) con (invoice_items) lo indicamos con la
+	 * anotación @JoinColumn(nombre_de_la_columna), es decir que vamos a tener un
+	 * campo o llave forana "invoice_id" en la tabla "invoice_items"(en la tabla de
+	 * la relación)
+	 */
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "invoice_id")
+	List<InvoiceLineItem> items;
+
+	public Invoice() {
+		this.items = new ArrayList<InvoiceLineItem>();
+	}
 
 	@PrePersist
 	public void prePersist() {
@@ -81,6 +104,18 @@ public class Invoice implements Serializable {
 
 	public void setClient(Client client) {
 		this.client = client;
+	}
+
+	public List<InvoiceLineItem> getItems() {
+		return items;
+	}
+
+	public void setItems(List<InvoiceLineItem> items) {
+		this.items = items;
+	}
+
+	public void addInvoiceLineItem(InvoiceLineItem item) {
+		this.items.add(item);
 	}
 
 	private static final long serialVersionUID = 1L;
