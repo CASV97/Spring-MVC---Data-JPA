4f6af416b2dc95c18930b1257b791da6f028a937
Christan Ariel Salazar Verduga <ariel-2h@hotmail.com>
2020-12-20 17:33:11 +0100

    Analizando y revisando las tablas y relaciones en MySQL Workbench
    Vamos a ver los datos, vamos a comprobar que se hayan guardado la tabla correctamente segun la relacion, pero antes de ejecutar vamos a agregar algunos insert en la tabla productos para tener algunos ejemplos, se ha agregado el atributo orphanRemoval para eliminar registros que no están asociados con sus correspondientes relaciones.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,25 +1,35 @@
 /*populate tables, utilizndo H2 base de datos de prueba e insertando datos en la tabla clients, es importante que este archivo se llave import.sql ya que JPA va a buscar el archivo con este nombre */
 INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(1, 'Pedro', 'Guzman','pguzman@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(2, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(2, 'El Peter', 'Velez','jvelz@gmail.com','2020-11-13','');
 INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(3, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
 INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(4, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
 INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(5, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(6, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(7, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(8, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(9, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(10, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(11, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(12, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(13, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(14, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(15, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(16, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(17, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(18, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(19, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(20, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(21, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(22, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(23, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
-INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(24, 'Jonh', 'Velez','jvelz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(6, 'Jonh', 'Velez','jvealz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(7, 'Jonh', 'Velez','jvealz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(8, 'Jonh', 'Vel1z','jvealz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(9, 'Jonh', 'Vel1z','jvealz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(10, 'Jonh', 'Ve1ez','jvaelz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(11, 'Jonh', 'Ve1ez','jvaelz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(12, 'Jasnh', 'V1lez','jvelz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(13, 'Jasnh', 'V1lez','jselz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(14, 'Jasnh', 'Velez','jselz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(15, 'Jasnh', 'Velez','jselz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(16, 'Jasnh', 'Velez','jselz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(17, 'Jasnh', 'Veflez','jselz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(18, 'Jasnh', 'Veflez','jselz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(19, 'Jasnh', 'Veflez','jvelz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(20, 'Jo5h', 'Velfez','jvqlz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(21, 'Jo5h', 'Velfez','jvqlz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(22, 'Jo5h', 'Velez','jvqlz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(23, 'Jo5h', 'Velez','jvqlz@gmail.com','2020-11-13','');
+INSERT INTO clients(id,first_name,last_name,email,create_at,photo) VALUES(24, 'Jo5h', 'Velez','jvqlz@gmail.com','2020-11-13','');
+
+/* Populate products table*/
+
+INSERT INTO products(name,price,create_at) VALUES('Panasonic Pantalla LCD', 255.50 ,NOW());
+INSERT INTO products(name,price,create_at) VALUES('Sony Camara digital',500,NOW());
+INSERT INTO products(name,price,create_at) VALUES('Apple ipod shuffle',999.99,NOW());
+INSERT INTO products(name,price,create_at) VALUES('Sony Notebook z110',430,NOW());
+INSERT INTO products(name,price,create_at) VALUES('Hewlerr packard Multifuncional D220',456,NOW());
+INSERT INTO products(name,price,create_at) VALUES('Toshiba notebook asd1234', 330,NOW());
+INSERT INTO products(name,price,create_at) VALUES('Mica Comoda 434 cajones', 4561,NOW());
