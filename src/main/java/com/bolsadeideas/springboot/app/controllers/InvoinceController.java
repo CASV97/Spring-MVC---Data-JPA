60b7523934c6edbdecff82a16e34c0d96a537ed5
Christan Ariel Salazar Verduga <ariel-2h@hotmail.com>
2021-02-12 01:08:00 +0100

    Validando el formulario de la factura
    Vamos a validar el formulario de la factura, al menos se tiene que validar el campo descripción y además que
    el detalle de la factura al menos contenga una línea y a dar un mensaje de error customizado
    En el campo de la vista descipción agregamos un par de clases para la validacíon desde el input y agregar  el mensaje de error desde el contenido

--- src/main/java/com/bolsadeideas/springboot/app/controllers/InvoinceController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/InvoinceController.java
@@ -2,11 +2,14 @@ package com.bolsadeideas.springboot.app.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -84,9 +87,20 @@ public class InvoinceController {
 	 * 
 	 */
 	@PostMapping("/form")
-	public String save(Invoice invoice, @RequestParam(name = "item_id[]", required = false) Long[] itemId,
+	public String save(@Valid Invoice invoice, BindingResult result, Model model,
+			@RequestParam(name = "item_id[]", required = false) Long[] itemId,
 			@RequestParam(name = "quantity[]", required = false) Integer[] quantity, RedirectAttributes flash,
 			SessionStatus status) {
+		if (result.hasErrors()) {
+			model.addAttribute("title", "Create Invoice");
+			return "invoice/form";
+
+		}
+		if (itemId == null || itemId.length == 0) {
+			model.addAttribute("title", "Create Invoice");
+			model.addAttribute("error", "Error: The invoice cannot be empty, don´t have any lines");
+			return "invoice/form";
+		}
 		for (int i = 0; i < itemId.length; i++) {
 			Product product = clientService.findProductById(itemId[i]);
 			InvoiceLineItem line = new InvoiceLineItem();
