afafdeb97d92177f89b0140fa077723091cdecff
Christan Ariel Salazar Verduga <ariel-2h@hotmail.com>
2021-10-12 22:07:16 +0200

    Añadiendo método configure(HttpSecurity http) para las reglas ACL en las rutas
    Continuando con la configuracion de Spring security, vamos a implementar un nuevo
    metodo para las autorizaciones http (rutas), para dar seguridad a nuestras paginas
    por ejemplo al CRUD de clientes, implementamos el método de la clase padre
    WebSecurityConfigurerAdapter, vamos a sobreescribir el método configura(HttpSecurity)
    aqui haremos uso del objeto http para declarar los accesos a las diferentes rutas
    de la aplicacion, tambien podremos filtrar los accesos por roles.

--- src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
@@ -21,6 +22,25 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	/**
+	 * Añadiendo método configure(HttpSecurity http) para las reglas ACL en las
+	 * rutas invocando el objeto http vamos a invocar el metodo authorizeRequest() y
+	 * para comenzar con el método antMatchers, para asignar nuestras rutas, todo lo
+	 * que sea de acceso publico, con permitAll(), para las rutas privadas o que
+	 * queremos proteger
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/list").permitAll()
+				.antMatchers("/show/**").hasAnyRole("USER").
+				antMatchers("/uploads/**").hasAnyRole("USER")
+				.antMatchers("/form/**").hasAnyRole("ADMIN")
+				.antMatchers("/delete/**").hasAnyRole("ADMIN")
+				.antMatchers("/invoice/**").hasAnyRole("ADMIN")
+				.anyRequest().authenticated();
+	}
+
 	/**
 	 * Registramos el password encoder como por defecto en nuestra configuracion de
 	 * spring security, con este password encoder podemos crear los usuarios y
